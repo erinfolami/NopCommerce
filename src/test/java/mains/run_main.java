f7ff538ccacad1d90ce97b725a751f785273643f
erinfolami <emmanuelerinfolami2@gmail.com>
2021-03-20 08:36:56 +0100

    removed empty files

--- src/test/java/mains/run_main.java
+++ src/test/java/mains/run_main.java
@@ -3,6 +3,7 @@ package mains;
 
 import com.NopCommerce.utils.ReadConfig;
 import com.NopCommerce.utils.Screenshot;
+import com.NopCommerce.utils.XLUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,11 +12,14 @@ import java.io.IOException;
 
 public class run_main {
     public static void main(String[] args)  {
-        ReadConfig readConfig = new ReadConfig();
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + readConfig.get_chromepath());
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://web.facebook.com/?_rdc=1&_rdr");
-        Screenshot.get_screenshot(driver,"test sshello");
+//        ReadConfig readConfig = new ReadConfig();
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + readConfig.get_chromepath());
+//        ChromeDriver driver = new ChromeDriver();
+//        driver.get("https://web.facebook.com/?_rdc=1&_rdr");
+
+        //read excel
+        XLUtils xl = new XLUtils();
+
 
     }
 }
